476c9e315fac6aedce79d22d896d7567168d487d
saraeltlt <47868870+saraeltlt@users.noreply.github.com>
2023-03-29 21:50:26 +0200

    units edits

--- app/src/main/java/com/example/myweatherapp/notifications/notificationview/NotificationFragment.kt
+++ app/src/main/java/com/example/myweatherapp/notifications/notificationview/NotificationFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myweatherapp.ConfirmDeleteInterface
+import com.example.myweatherapp.ConfirmDialogFragment
 import com.example.myweatherapp.utils.MyApp
 import com.example.myweatherapp.R
 import com.example.myweatherapp.databinding.FragmentNotificationBinding
+import com.example.myweatherapp.favourite.favView.FavoriteFragment
 import com.example.myweatherapp.notifications.notificationmodel.Alert
 import com.example.myweatherapp.notifications.notificationviewmodel.NotificationViewModel
 import com.example.myweatherapp.notifications.notificationviewmodel.NotificationViewModelFactory
@@ -21,7 +25,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 
-class NotificationFragment : Fragment() , OnNotifClickListner{
+class NotificationFragment : Fragment() , OnNotifClickListner, Dialoge.SaveAlertInterface, ConfirmDeleteInterface{
     private lateinit var binding: FragmentNotificationBinding
     private lateinit var dialog: Dialoge
     lateinit var viewModel: NotificationViewModel
@@ -74,13 +78,27 @@ class NotificationFragment : Fragment() , OnNotifClickListner{
         return view
     }
  fun   showAlertDialog(){
-     dialog= Dialoge(requireContext())
+     dialog= Dialoge(requireContext(),this)
      dialog.show(parentFragmentManager, "alertDialog")
 
  }
 
     override fun onDeleteClick(alert: Alert) {
-        TODO("Not yet implemented")
+     //   myalert=alert
+        var dialog = ConfirmDialogFragment(this)
+        dialog.show(childFragmentManager, "alertDialog")
+    }
+
+    override fun onClickSave() {
+        //input alert
+        // viewModel.SaveAlert()
+    }
+
+    override fun onClick(confirmDelete: Boolean) {
+        if (confirmDelete) {
+           // viewModel.deleteAlert(myAlert)
+            Toast.makeText(requireContext(),R.string.removedFromAlerts, Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/com/example/myweatherapp/startPref/model/MyPref.kt
+++ app/src/main/java/com/example/myweatherapp/startPref/model/MyPref.kt
@@ -3,7 +3,7 @@ package com.example.myweatherapp.startPref.model
 import com.google.android.gms.maps.model.LatLng
 
 data class MyPref(var appLanguage: String = "en",
-             var appUnit: String ="metric",
+             var appUnit: String ="standard",
              var appMode: String = "dark",
              var myLocation: LatLng =LatLng(0.0,0.0)
 ) {
